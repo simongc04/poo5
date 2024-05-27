45b29018b777d15711dfdc5905eb7a947be8ee2b
simon <simonescuela9@gmail.com>
2024-05-27 12:43:17 +0100

    prueba 28

---
+++ prueba28/utilidadString.py
@@ -0,0 +1,24 @@
+DIGITOS = ["0","1","2","3","4","5","6","7","8","9"]
+
+
+class UtilidadString:
+
+
+
+    #funcion que devuelva el numero de digitos de un string
+    @staticmethod
+    def contarDigitos(lacadena:str) -> int:
+        resultado = 0
+        for car in range(0, len(lacadena)):
+            if lacadena[car] in DIGITOS:
+                resultado += 1
+
+        return resultado
+    
+
+
+if __name__ == "__main__":
+    def main():
+        print(UtilidadString.contarDigitos("estoes1y esto2"))
+
+    main()
--- test/hgt/test/test_tarea.py
+++ test/hgt/test/test_tarea.py
@@ -1,1 +1,14 @@
 from tarea import Tarea
+
+
+def tarea(Tarea):
+    self.mi_tarea = Tarea(1, "Hacer compras", False)
+
+
+def test_read(Tarea):
+
+
+def test_update(Tarea):
+
+
+def test_delete(Tarea):
